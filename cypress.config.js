fb6c5633f0fbc734a36667320abd74930a3ec7b8
nir-tech <111503458+nir-tech@users.noreply.github.com>
2022-12-05 19:36:09 +0200

    inital commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-
     },
-    env: {
-
-      paths: [
-        'lp/plus/',
-        'lp/perfect',
-        'lp/moving-perfect',
-        'lp/packages/',
-        'lp/stingtv-netflix/',
-        'lp/sting-ds/',
-        'lp/samsung-stingtv/'
-      ]
-    }
-
-
+    video: false,
+    screenshotOnRunFailure: false,
   },
 });
--- cypress/e2e/stingQueryParamNumberChange.cy.js
+++ cypress/e2e/stingQueryParamNumberChange.cy.js
@@ -1,41 +1,36 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false;
+});
+
 describe('Test that campaign_source query param changes phone number', () => {
   before('Load query paramters and related numbers from fixtures', () => {
+    cy.fixture('queryParamsAndNumbers.json').as('queryParamsAndNumbers');
+    cy.fixture('paths.json').as('paths');
+  });
 
-    cy.fixture('queryParamsAndNumbers.json').as('queryParamsAndNumbers')
-    cy.fixture('paths.json').as('paths')
-
-  })
-
-
-  it.only('Loop through all paths with relevant query params', function () {
-
-    // Declare variables from fixtures
-    const queryParamsAndNumbers = this.queryParamsAndNumbers.objects
-    const paths = this.paths.objects
+  it('Loop through all paths with relevant query params', function () {
+    const queryParamsAndNumbers = this.queryParamsAndNumbers.objects;
+    const paths = this.paths.objects;
 
     for (let i of queryParamsAndNumbers) {
+      i = queryParamsAndNumbers.indexOf(i);
 
-      i = queryParamsAndNumbers.indexOf(i)
+      paths.forEach((path) => {
+        cy.log('Current Path:', path);
+        cy.log('Current QueryParam:', queryParamsAndNumbers[i].queryParam);
+        cy.log('Current Phone Number:', queryParamsAndNumbers[i].phoneNumber);
 
-      paths.forEach(path => {
-
-        cy.log('Current Path: ', path)
-        cy.log('Current QueryParam: ', queryParamsAndNumbers[i].queryParam)
-        cy.log('Current Phone Number: ', queryParamsAndNumbers[i].phoneNumber)
-
-        cy.visit(('/') + path, {
-
+        // The baseUrl is configured from a script inside package.json
+        cy.visit('/' + path, {
           qs: {
-            campaign_source: queryParamsAndNumbers[i].queryParam
-          }
-        })
+            campaign_source: queryParamsAndNumbers[i].queryParam,
+          },
+        });
 
-        cy.get('#phoneNumber').should('have.text', queryParamsAndNumbers[i].phoneNumber)
-
-      })
+        cy.get('#phoneNumber')
+          .should('be.visible')
+          .and('have.text', queryParamsAndNumbers[i].phoneNumber);
+      });
     }
-  })
-})
-
-
-
+  });
+});
